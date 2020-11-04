e64a911f7255752deeb66732ac32f0ab311da2e3
Anthony Leon <acidknights89@gmail.com>
2020-11-04 06:31:11 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Ch
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 8, 'M', 'black', 'aggressive', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 5, 'F', 'light brown', 'calm', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Bown', 12, 'M', 'brown', 'playful', 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, 'F', 'white', 'aggressive', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, 'F', 'white', 'goofy', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'brown', 'calm', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 10, 'F', 'light brown', 'aggressive', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, 'M', 'brown', 'aggressive', 0);
